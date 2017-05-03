e9872fd1599fafc65bc9f9268b4a2bd939fc6b8c
Nana Adjei Manu <claeusdev@gmail.com>
2017-05-02 23:24:51 -0400

    Add adsense

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,8 @@
-
-products.delete
+electronics_category = Category.where(name: "Electronics").first_or_create(name: "Electronics")
+men_category = Category.where(name: "Men").first_or_create(name: "Men")
+women_category = Category.where(name: "Women").first_or_create(name: "Women")
+baby_category = Category.where(name: "Baby & Kids").first_or_create(name: "Baby & Kids")
+home_category = Category.where(name: "Home & Furniture").first_or_create(name: "Home & Furniture")
+beauty_category = Category.where(name: "Beauty & Health").first_or_create(name: "Beauty & Health")
+grocery_category = Category.where(name: "Grocery").first_or_create(name: "Grocery")
+autos_category = Category.where(name: "Autos & Vehicles").first_or_create(name: "Autos & Vehicles")
